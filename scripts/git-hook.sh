8adb7771c58927e9aae0279813fccb1d70ec7b89
kmlee78 <jaoming787@gmail.com>
2022-05-22 15:34:33 +0900

    chore: add commit message hook

--- scripts/git-hook.sh
+++ scripts/git-hook.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# pre-commit hook install from pip
 pre-commit install
+
+# symlink
+ln -sf ../../scripts/commit-msg-hook.py .git/hooks/commit-msg
+
+# implement permission
+chmod +x .git/hooks/commit-msg
